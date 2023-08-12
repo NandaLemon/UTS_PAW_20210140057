6051ce29a307bb0168fb25ffc3ed39ef049f2ae5
NandaLemon <rizkynandapr@gmail.com>
2023-08-12 11:54:09 +0700

    first

---
+++ src/main/java/com/uts/uts/tamuhotel.java
@@ -0,0 +1,14 @@
+public class tamuhotel {
+    public static void main(String[] args) {
+        var customer = new personP(); // Create an instance of CustomerHotel
+        customer.name = "Rizky Nanda";
+        customer.address = "Kendal";
+
+        customer.infoTamu();
+        customer.waktuCheckin("10:42 AM");
+        customer.waktuCheckout("12:54 PM");
+        customer.informasiPembayaran(570.000);
+        customer.pesananMakanan("Ayam Goreng");
+
+    }
+}
